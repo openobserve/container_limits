c25ad15f014e84dbd313fd6d0e04422e08b3b178
Prabhat Sharma <hi.prabhat@gmail.com>
2023-07-13 12:01:20 -0700

    use cgroup version 2

--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,40 @@ use std::fs;
 use std::io::ErrorKind;
 
 fn main() {
-    println!(" ------ Details from the host ------ ");
-
     let cpu_num = sys_info::cpu_num().unwrap();
     println!("Number of logical CPUs is {}", cpu_num);
 
     let load_avg = sys_info::loadavg().unwrap();
-    println!(
-        "Load average in the past 1, 5, and 15 minutes: {}, {}, {}",
-        load_avg.one, load_avg.five, load_avg.fifteen
-    );
+    println!("Load average in the past 1, 5, and 15 minutes: {}, {}, {}", load_avg.one, load_avg.five, load_avg.fifteen);
 
     let mem_info = sys_info::mem_info().unwrap();
     println!("Total RAM: {} KB", mem_info.total);
     println!("Free RAM: {} KB", mem_info.free);
     println!("Used RAM: {} KB", mem_info.total - mem_info.avail);
 
-    println!(" ------ Details from inside the container ------ ");
+    println!("Use control groups v2");
 
-    match fs::read_to_string("/sys/fs/cgroup/memory/memory.limit_in_bytes") {
-        Ok(memory_limit) => println!("Memory Limit: {}", memory_limit),
-        Err(ref error) if error.kind() == ErrorKind::NotFound => {
-            println!("Memory limit file not found, possibly not running in a container")
-        }
+    match fs::read_to_string("/sys/fs/cgroup/cpu.weight") {
+        Ok(cpu_weight) => println!("CPU Weight: {}", cpu_weight),
+        Err(ref error) if error.kind() == ErrorKind::NotFound => println!("CPU weight file not found, possibly not running in a container"),
         Err(error) => panic!("Unexpected error: {}", error),
     }
 
-    match fs::read_to_string("/sys/fs/cgroup/cpu/cpu.cfs_quota_us") {
-        Ok(cfs_quota_us) => println!("CPU Shares: {}", cfs_quota_us),
-        Err(ref error) if error.kind() == ErrorKind::NotFound => {
-            println!("CPU quota file not found, possibly not running in a container")
-        }
+    match fs::read_to_string("/sys/fs/cgroup/cpu.max") {
+        Ok(cpu_max) => println!("CPU Max: {}", cpu_max),
+        Err(ref error) if error.kind() == ErrorKind::NotFound => println!("CPU max file not found, possibly not running in a container"),
+        Err(error) => panic!("Unexpected error: {}", error),
+    }
+
+    match fs::read_to_string("/sys/fs/cgroup/memory.max") {
+        Ok(memory_max) => println!("Memory Max: {}", memory_max),
+        Err(ref error) if error.kind() == ErrorKind::NotFound => println!("Memory max file not found, possibly not running in a container"),
+        Err(error) => panic!("Unexpected error: {}", error),
+    }
+
+    match fs::read_to_string("/sys/fs/cgroup/memory.current") {
+        Ok(memory_current) => println!("Current Memory Usage: {}", memory_current),
+        Err(ref error) if error.kind() == ErrorKind::NotFound => println!("Current memory usage file not found, possibly not running in a container"),
         Err(error) => panic!("Unexpected error: {}", error),
     }
 }
